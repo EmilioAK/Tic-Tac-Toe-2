7e1aecca871defb56c1d5dd7560be32016aafe97
EmilioAK <emilio.alvarez96@gmail.com>
2021-12-05 16:53:00 +0100

    Update

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const ticTacToe = (function() {
-  let board = [];
+  const board = [];
   const metadata = {};
     
   const reset = () => {
-    board = Array.from(Array(9), x => null);
-    metadata.whosTurn = 0;
-    metadata.winner = null;
-  }
+    _.remove(board, () => true);
+    const emptyBoard = _.range(9).fill(null);
+    emptyBoard.forEach(e => {board.push(e)});
 
-  reset();
+    metadata.whosTurn = 'Circle';
+    metadata.winner = null;
+  };reset();
 
   const getBoard = () => [...board];
 
-  const getMetadata = () => Object.assign({}, metadata)
+  const getMetadata = () => ({...metadata});
 
   const gameWon = () => {
     const rows = () => _.chunk(board, 3);
@@ -33,7 +34,7 @@ const ticTacToe = (function() {
     return board.every(cell => cell != null) && !gameWon();
   };
 
-  const incrementTurn = () => {metadata.whosTurn = (metadata.whosTurn + 1) % 2};
+  const incrementTurn = () => {metadata.whosTurn = metadata.whosTurn === 'Circle' ? 'Cross' : 'Circle'};
 
   const gameOver = () => {  // Supposed to be run whenever a win can happen, currenlty only when place() is run
     if (tie()) {
@@ -44,6 +45,7 @@ const ticTacToe = (function() {
       incrementTurn();
     }
   };
+
   const place = (index) => {
     if (board[index] === null) {
       board[index] = metadata.whosTurn;
@@ -58,23 +60,17 @@ const cells = document.getElementsByClassName('cell');
 const winningMessage = document.getElementById('winningMessage');
 
 function render() {
-  const players = {
-    0: 'Circle',
-    1: 'Cross',
-    'Tie': 'Tie'
-  };
-
   const drawBoard = (() => {
     for (let index = 0; index < ticTacToe.getBoard().length; index++) {
       const gameCell = ticTacToe.getBoard()[index];
-      domCell = cells[index]
+      const domCell = cells[index]
   
       domCell.innerHTML = '';
-      if (gameCell === 0) {
+      if (gameCell === 'Circle') {
         const circleDiv = document.createElement('div');
         circleDiv.className = 'circle';
         domCell.appendChild(circleDiv);
-      } else if (gameCell === 1) {
+      } else if (gameCell === 'Cross') {
         const crossDiv = document.createElement('div');
         crossDiv.className = 'cross';
         domCell.appendChild(crossDiv);
@@ -85,7 +81,7 @@ function render() {
   const winnerDeclared = (() => {
     const winningPlayer = ticTacToe.getMetadata().winner;
     if (winningPlayer != null) {
-      document.getElementById('winner').textContent = winningPlayer === 'Tie' ? 'Tie!' : `${players[winningPlayer]} won the game!`;
+      document.getElementById('winner').textContent = winningPlayer === 'Tie' ? 'Tie!' : `${winningPlayer} won the game!`;
       winningMessage.style.display = 'flex';
     }
   })();
